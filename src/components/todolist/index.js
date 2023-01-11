caa2c6eff70a0d9d32d9bd49b04609ecabc3479b
NguyenDuyTu97 <45293691+NguyenDuyTu97@users.noreply.github.com>
2023-01-11 23:22:05 +0700

    Revert "add console"
    This reverts commit 88d7ec330bea06ab6be16805dd245203f11a9d7f.

--- src/components/todolist/index.js
+++ src/components/todolist/index.js
@@ -1,93 +1,91 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { genUuid, action } from "../../common";
-import {
-  addUser,
-  editUser,
-  deleteUser,
-  updateStatusUser,
-} from "../../actions/user";
-import Work from "../work";
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { genUuid, action, } from '../../common';
+import { addUser, editUser, deleteUser, updateStatusUser, } from '../../actions/user';
+import Work from '../work';
 
 function TodoList() {
-  //state of this component
-  const [userName, setUserName] = useState("");
-  const [userEdit, setUserEdit] = useState({});
-  const [typeAction, setTypeAction] = useState(action.ADD);
+    //state of this component
+    const [userName, setUserName] = useState('');
+    const [userEdit, setUserEdit] = useState({});
+    const [typeAction, setTypeAction] = useState(action.ADD);
 
-  //state of store
-  const users = useSelector((state) => {
-    //get state in store
-    // console.log(state, "state");
-    return state.user.list;
-  });
+    //state of store
+    const users = useSelector(state => { //get state in store
+        // console.log(state, "state");
+        return state.user.list;
+    });
 
-  const dispatch = useDispatch(); // call action in reducer of store
+    const dispatch = useDispatch(); // call action in reducer of store
 
-  function onChangeUserName(e) {
-    const { value } = e && e.target;
-    setUserName(value);
-  }
 
-  function handleAddUser() {
-    if (typeAction.ADD) {
-      let newUser = {
-        uuid: genUuid(),
-        name: userName,
-        status: Math.trunc(Math.random() * 3) + 1,
-      };
-
-      dispatch(addUser(newUser));
-      setUserName("");
-    } else {
-      let newUser = {
-        ...userEdit,
-        name: userName,
-      };
-
-      dispatch(editUser(newUser));
-      setUserEdit({});
-      setUserName("");
+    function onChangeUserName(e) {
+        const { value, } = e && e.target;
+        setUserName(value);
     }
-  }
 
-  function handleEditUser(user) {
-    setTypeAction(action.EDIT);
-    setUserName(user.name);
-    setUserEdit(user);
-  }
+    function handleAddUser() {
+        if (typeAction.ADD) {
+            let newUser = {
+                uuid: genUuid(),
+                name: userName,
+                status: Math.trunc(Math.random() * 3) + 1,
+            };
 
-  function handleDeleteUser(uuid) {
-    let confirm = window.confirm("Do you delete this user?");
-    if (confirm) dispatch(deleteUser(uuid));
-  }
+            dispatch(addUser(newUser));
+            setUserName('');
+        }
+        else {
+            let newUser = {
+                ...userEdit,
+                name: userName,
+            };
 
-  function handleChangeStatusUser(user) {
-    dispatch(updateStatusUser(user));
-  }
+            dispatch(editUser(newUser));
+            setUserEdit({});
+            setUserName('');
+        }
+    }
 
-  console.log("123456789");
+    function handleEditUser(user) {
+        setTypeAction(action.EDIT);
+        setUserName(user.name);
+        setUserEdit(user);
+    }
 
-  return (
-    <div className="container">
-      <div id="heading">
-        <input
-          name="username"
-          value={userName}
-          onChange={(e) => onChangeUserName(e)}
-        />
-        <button onClick={handleAddUser}>Save</button>
-      </div>
-      <div id="main">
-        <Work
-          users={users}
-          handleEditUser={handleEditUser}
-          handleDeleteUser={handleDeleteUser}
-          handleChangeStatusUser={handleChangeStatusUser}
-        />
-      </div>
-    </div>
-  );
+    function handleDeleteUser(uuid) {
+        let confirm = window.confirm("Do you delete this user?");
+        if (confirm) dispatch(deleteUser(uuid));
+    }
+
+    function handleChangeStatusUser(user) {
+        dispatch(updateStatusUser(user))
+    }
+
+    return (
+        <div className="container">
+            <div id="heading">
+                <input
+                    name='username'
+                    value={userName}
+                    onChange={(e) => onChangeUserName(e)}
+                />
+                <button
+                    onClick={handleAddUser}
+                >
+                    Save
+                </button>
+            </div>
+            <div id="main">
+                <Work
+                    users={users}
+                    handleEditUser={handleEditUser}
+                    handleDeleteUser={handleDeleteUser}
+                    handleChangeStatusUser={handleChangeStatusUser}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default TodoList;
