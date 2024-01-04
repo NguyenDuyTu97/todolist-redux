fe00c9887c5a175f0472b0703fad1a2d13fcc8be
NguyenDuyTu97 <duytu097@gmail.com>
2024-01-04 23:12:58 +0700

    test revert title 1

--- src/components/todolist/index.js
+++ src/components/todolist/index.js
@@ -65,7 +65,6 @@ function TodoList() {
     return (
         <div className="container">
             <h2>This is todo list redux javascript - TEST REVERT</h2>
-            <div>TEST REVERT CODE 2</div>
             <div id="heading">
                 <input
                     name='username'
