b697d7d4b5586a4ccda6c727d27193e5f8a2acf4
DuyTu97 <duytu097@gmail.com>
2024-03-10 17:33:50 +0700

    test conflict merge

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import TodoList from './components/todolist';
 
 function App() {
+  console.log("test conflict 17321003")
   return (
     <div className="">
       <TodoList />
