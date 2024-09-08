ec4597bf4a4501137916dbe1572c0ce3e4f48639
DuyTu97 <duytu097@gmail.com>
2024-09-09 00:11:30 +0700

    'deploy2'

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TodoList from './components/todolist';
 function App() {
   return (
     <div className="">
+      <h1>Test resize</h1>
       <TestResize />
       {/* <TodoList /> */}
     </div>
